d61874d4b45912f6fdae9a985697fdf0379634cc
mandara11 <mandaram2345@gmail.com>
2022-09-07 20:21:05 +0530

    CreatingListOfProductReview

---
+++ Management.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+
+        // UC1:- Product Review Management
+        public void IterateProductReview(List<ProductReview> productReviewList)
+        {
+            foreach (ProductReview list in productReviewList) //print list item
+            {
+                Console.WriteLine("ProductId:-" + list.ProductId + "  " + "UserId:-" + list.UserId + "  " + "Rating:-" + list.Rating
+                    + "  " + "Review:-" + list.Review + "  " + "IsLike:-" + list.IsLike); //Print data
+            }
+        }
+    }
+}
